057d1ed90366edfe0db82e80f09fd6ec62fa1ff5
Alex D <alexdailida@gmail.com>
2015-08-23 01:00:23 -0700

    added stubs for main pages

---
+++ ios/Peggy/Peggy/Component/Login/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  Peggy
+//
+//  Created by Alex D on 23/08/15.
+//  Copyright (c) 2015 Alex D. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: "LoginView", bundle: nil)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func loginWithFacebook(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: { () -> Void in
+            
+        })
+    }
+    
+}
