a5ce5450e1d2f7c65da72997cab94a044dd0f27f
Alex D <alexdailida@gmail.com>
2015-08-22 20:10:33 -0700

    initial commit

---
+++ ios/Peggy/Peggy/Component/Dashboard/DashboardViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Peggy
+//
+//  Created by Alex D on 22/08/15.
+//  Copyright (c) 2015 Alex D. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
