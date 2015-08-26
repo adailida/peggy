3d9dc7cee709a178b843e984937bcdf0e4ef2ac4
Alex D <alexdailida@gmail.com>
2015-08-25 22:44:05 -0700

    dashboard page stub

--- ios/Peggy/Peggy/Component/Dashboard/DashboardViewController.swift
+++ ios/Peggy/Peggy/Component/Dashboard/DashboardViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class DashboardViewController: UIViewController {
+    
+    @IBOutlet weak var upcomingEventsTableView: UITableView!
+    @IBOutlet weak var recommendationsTableView: UITableView!
+    
+    private var upcomingEventsDatasource = UpcomingEventsDatasource()
+    private var recommendationsDatasource = RecommendationsDatasource()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.upcomingEventsTableView.dataSource = self.upcomingEventsDatasource
+        self.recommendationsTableView.dataSource = self.recommendationsDatasource
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,43 @@ class DashboardViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
 
+class UpcomingEventsDatasource: NSObject, UITableViewDataSource {
+    
+    private var datasource: [UpcomingEvent] = [
+        UpcomingEvent(description: "Some event1"),
+        UpcomingEvent(description: "Some event2")
+    ]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datasource.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("UpcomingEventCell")!
+        let event = self.datasource[indexPath.row]
+        cell.textLabel?.text = "\(event.description)"
+        return cell
+    }
+}
+
+class RecommendationsDatasource: NSObject, UITableViewDataSource {
+    
+    private var datasource: [Recommendation] = [
+        Recommendation(description: "Some recommendation1"),
+        Recommendation(description: "Some recommendation2")
+    ]
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return datasource.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("RecommendationsCell")!
+        let recommendation = self.datasource[indexPath.row]
+        cell.textLabel?.text = "\(recommendation.description)"
+        return cell
+    }
 }
 
---
+++ ios/Peggy/Peggy/Model/UpcomingEvent.swift
@@ -0,0 +1,16 @@
+//
+//  UpcomingEvent.swift
+//  Peggy
+//
+//  Created by Alex D on 25/08/15.
+//  Copyright © 2015 Alex D. All rights reserved.
+//
+
+import Foundation
+
+
+struct UpcomingEvent {
+    
+    var description: String
+    
+}
